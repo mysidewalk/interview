822566feb479154464db6a9f6f27d475f8c7135a
Robert Fines <robert@localruckus.com>
2014-03-05 21:53:29 -0600

    completed the questions

--- PythonQuestions/python_questions/hard.py
+++ PythonQuestions/python_questions/hard.py
@@ -3,9 +3,12 @@
     defined below.
 """
 def question1():
+    import dis as d
     def test():
         return {'a': 1, 'b': 2}
     #print disassembly here, you may wish to import items in the stdlib if necessary
+    d.dis(test)
+    
 
 """ Using the predefined list, "test_list", group the list
     into new lists by the first letter of each item in the list.
@@ -14,8 +17,16 @@ def question1():
 """
 def question2():
     test_list = ["alligator", "bear", "cat", "crocodile", "beaver", "ape", "bee", "coati", "aardvark"]
-    grouped_list = None
+    grouped_list = []
     # group the list by first letter here, you may wish to import items in the stdlib if necessary
+    intermediate = {}
+    for word in test_list: 
+        if word[0] in intermediate.keys():
+            intermediate[word[0]].append(word)
+        else:
+            intermediate[word[0]] = [word]
+    [grouped_list.append(x) for x in intermediate.values()]
+    print grouped_list
     return grouped_list
 
 """ Create a class, "TinyObject", which uses up a very small amount of memory per instance.
@@ -26,3 +37,6 @@ def question3():
     TinyObject = None
     # create the class here, you may wish to import items in the stdlib if necessary
     return TinyObject
+
+question2()
+question1()
