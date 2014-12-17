875d8e41cdae78bd8c7bb01f968717a07f217d77
Peter Freeze <pfreeze@mindmixer.com>
2014-12-17 13:55:54 -0600

    Updated formatting to match the MindMixer Javascript style.

--- JavascriptQuestions/questions.js
+++ JavascriptQuestions/questions.js
@@ -1,89 +1,78 @@
-/* Class definition
- * Define a class, 'Foo', with a class level attribute 'bar',
+/* Class Definition
+ * Define a class, 'Foo',
+ * with a class level attribute 'bar',
  * an instance level variable 'baz',
- * and a method 'biz' that takes one
- * argument and simply returns the value of that argument.
+ * and a method 'biz' that takes one argument and simply returns the value of that argument.
  */
+var question1 = function() {
+    var Foo = null;
+    // Define class Foo here
+    
+    return Foo;
+};
  
- function question1() {
-	var Foo = null;
-	// Define class Foo here
-	
-	return Foo;
- }
- 
- /* Regex and String sanitization
- * write a function that takes a single input and uses
- * replace to remove all non-alphanumeric characters 
- * from that input
+/* RegEx and String Sanitation
+ * Write a function that takes a single string input and uses
+ * replace to remove all non-alphanumeric characters from that input
  * for example 'a@b$c^d!1,2.3/4' => 'abcd1234'
  */
-function question2(inputString)
-{
-	var result = "";
-	// put the function here
-	
-	return result;
-}
+var question2 = function(inputString) {
+    var result = '';
+    // Put the function here
+    
+    return result;
+};
 
-/*
- * Define a function that prints the numbers from 1 to 100. 
+/* Define a function that prints the numbers from 1 to 100. 
  * For multiples of three print "Fizz" instead of the number
  * and for multiples of five print "Buzz". For numbers which
  * are multiples of both three and five print "FizzBuzz".
  */
-function question3()
-{
-	// implement function body here, no need to return a value,
+var question3 = function() {
+    // implement function body here, no need to return a value,
     // "document.write" statements will work fine
-	
-    return;
-}
+};
 
-/* 
- * Argument handling
- * Write a function that takes an arbirary number of numeric arguments
- * and return the sum of those arguments
+/* Argument Handling
+ * Write a function that takes an arbitrary number of numeric arguments
+ * and returns the sum of those arguments.
  */
-function question4()
-{
-	var sum = null;
-	// define the sum function here
-	
-	return sum;
-}
+var question4 = function() {
+    var sum = null;
+    // Define the sum function here
+    
+    return sum;
+};
 
-/*
- * Using the predefined list, "test_list", group the list
+/* Using the predefined list, "testList", group the list
  * into new lists by the first letter of each item in the list.
  * Your final solution should return a list of lists whose members
  * all have the same first letter.
  * (Feel free to use underscore or another preferred library for this kind of processing)
  */
-function question5()
-{
-	var test_list = ['alligator', 'bear', 'cat', 'crocodile', 'beaver', 'ape', 'bee', 'coati', 'aardvark'];
-	var grouped_list = null;
-	// group the list by first letter here
-	
-   	return grouped_list;
-} 
+var question5 = function() {
+    var testList = ['alligator', 'bear', 'cat', 'crocodile', 'beaver', 'ape', 'bee',
+        'coati', 'aardvark'];
+    var groupedList = null;
+    // Group the list by first letter here
+    
+    return groupedList;
+};
 
-/*
- * map & reduce
+/* Map & Reduce
  * In text processing, letters and combinations of letters are
  * often analyzed using numerical interpretations. Given a rule for
  * calculating the numerical "total value" of a string, wherein each letter of the string is worth
- * its ascii value (i.e. letter.charCodeAt(0) for each letter) and then summed, use map & reduce
- * methodology to calculate the "total value" of each string in the array "names", sum the "total values",
- * and then calculate the average.
+ * its ASCii value (i.e. letter.charCodeAt(0) for each letter) and then summed, use map & reduce
+ * methodology to calculate the "total value" of each string in the array "names", sum the
+ * "total values", and then calculate the average.
  * (Feel free to use underscore or another preferred library for this kind of processing)
  */
- function question6()
- {
-	var averageTotalValue = null;
-	var names = ["David", "Jimmy", "Matt", "Andy", "Nick", "Nathan", "Andrea", "Stephen", "Justin", "Emily", "Hannah"];
-	// apply your map and reduce strategy here
+var question6 = function() {
+    var averageTotalValue = null;
+    var names = ['David', 'Jimmy', 'Matt', 'Andy', 'Nick', 'Nathan', 'Andrea', 'Stephen',
+        'Justin', 'Emily', 'Hannah'];
+    // Apply your map and reduce strategy here
 
-	return averageTotalValue;
- }
+    return averageTotalValue;
+};
